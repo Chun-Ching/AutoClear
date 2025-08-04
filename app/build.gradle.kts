6957963870c0864ffcde37b33b2c6b8840f00403
Tommy <tommycheng@thinkingsoftware.tw>
2025-08-04 14:19:43 +0800

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,29 +19,42 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    signingConfigs {
+        // Kotlin DSL 中，使用 create 方法來定義新的 signingConfigs
+        create("release") {
+            storeFile(file(System.getenv("KEYSTORE_FILE_PATH")))
+            storePassword(System.getenv("KEYSTORE_PASSWORD"))
+            keyAlias(System.getenv("KEY_ALIAS"))
+            keyPassword(System.getenv("KEY_PASSWORD"))
         }
     }
+
+    buildTypes {
+        // Kotlin DSL 中，使用 getByName 方法來存取已有的 build type
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            // 在 Kotlin DSL 中，存取 signingConfigs 需使用 getByName 方法
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    // 這幾個區塊必須在 android { ... } 內部
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
